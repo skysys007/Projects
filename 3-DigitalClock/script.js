c2667e3f50ca656594cb1e1c50e94cc2137aaaca
skysys007 <gagan07pradhan@gmail.com>
2025-09-08 17:29:21 +0530

    Initial commit

---
+++ 3-DigitalClock/script.js
@@ -0,0 +1,10 @@
+let time_box = document.querySelector('#clock')
+
+setInterval(function () {
+    let hours = new Date().getHours() - 12;
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    let time = `${hours} : ${minutes} : ${seconds}`
+    console.log(time);
+    time_box.innerHTML = time;
+}, 1000)
